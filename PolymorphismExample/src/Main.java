0e4848b68e345d99fdece326f8212a4aa7dcb2db
ThomasCanning <thomas500book@gmail.com>
2021-02-07 12:39:34 +0000

    Polymorphism Examples

--- PolymorphismExample/src/Main.java
+++ PolymorphismExample/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         SportsCar porsche911 = new SportsCar();
         SuperCar lamborghiniHuracan = new SuperCar();
         PickupTruck teslaCybertruck = new PickupTruck();
