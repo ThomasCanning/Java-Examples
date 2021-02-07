f85930e0fa2706529b68ee0f8f4289c84e6a58f3
ThomasCanning <thomas500book@gmail.com>
2021-02-07 17:15:32 +0000

    Testing out access modifiers

---
+++ src/modifierexamples/TestClass2.java
@@ -0,0 +1,13 @@
+package modifierexamples;
+
+public class TestClass2 {
+
+    public static void main(String[] args) {
+        System.out.println(TestClass.publicNumber);/*As TestClass is public, the
+        variable public number can be accessed by a different class in the same package, but not in a different
+        package as TestClass has the default modifier so can't be accessed out of the package*/
+
+        System.out.println(TestClass.defaultNumber);
+
+    }
+}
