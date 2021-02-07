5afaeccdf6b30ac7f0ff89ce3eb2af766cff7104
ThomasCanning <thomas500book@gmail.com>
2021-02-07 19:56:08 +0000

    Examples about the non-access modifier abstract

--- src/modifierexamples/TestingNonAccessModifiers.java
+++ src/modifierexamples/TestingNonAccessModifiers.java
@@ -1,4 +1,30 @@
 package modifierexamples;
 
-public class TestingNonAccessModifiers {
+final class TestingNonAccessModifiers {
+    static void exampleMethod() {
+        System.out.println("exampleMethod has been run");
+    }
+
+    int defaultInt = 10;
+    final int finalInt = 20;
+
+
+/* class Inheritance extends TestingNonAccessModifiers{} Would generate an error as the non-access modifier
+final means that the class cannot be inherited by other classes
+ */
+
+    public static void main(String[] args) {
+        TestingNonAccessModifiers object = new TestingNonAccessModifiers();
+        object.exampleMethod();
+        object.defaultInt = 50;
+        System.out.println(object.defaultInt);
+
+        //object.finalInt = 50;
+        //System.out.println(object.finalInt);
+        /*The above code would fail, as you cannot assign a value to a final varaible */
+
+    }
 }
+
+
+
