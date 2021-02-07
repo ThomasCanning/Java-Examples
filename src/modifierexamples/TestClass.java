45b7a4795dc1d74570cdd46c3b7462ed70c5c40d
ThomasCanning <thomas500book@gmail.com>
2021-02-07 17:41:32 +0000

    Adding access modifiers

---
+++ src/modifierexamples/TestClass.java
@@ -0,0 +1,20 @@
+package modifierexamples;
+
+class TestClass {
+    public static int publicNumber = 20;  /*The static keyword allows the variable to be accessed from
+    within the main method without an object of the class needed to be created*/
+
+    private static int privateNumber = 25;
+    protected static int protectedNumber = 30;
+    static int defaultNumber = 10;
+
+    public static void main(String[] args) {
+        System.out.println(publicNumber);
+        System.out.println(privateNumber);
+        System.out.println(protectedNumber);
+        /*As the main method is within the class, the variables with all access modifiers can be accessed*/
+
+
+    }
+
+}
