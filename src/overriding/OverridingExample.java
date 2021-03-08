982ef42fcb569afe32a52fd7b4a862e9522305ec
ThomasCanning <thomas500book@gmail.com>
2021-03-08 18:45:58 +0000

    Overriding

--- src/overriding/OverridingExample.java
+++ src/overriding/OverridingExample.java
@@ -1,4 +1,24 @@
 package overriding;
 
-public class OverridingExample {
+class OverridingExample {
+
+    static class Animal{
+        static void age(int animalAge) {
+            System.out.println("This animal is " + animalAge + " years old");
+        }
+    }
+
+    static class Lion extends Animal {
+        static void age(int animalAge) { System.out.println("This Lion is " + animalAge + " years old");}
+    }
+
+    public static void main(String[] args) {
+        Animal genericAnimal = new Animal();
+        Lion lion = new Lion();
+
+        genericAnimal.age(10);
+        lion.age(8);
+
+
+    }
 }
