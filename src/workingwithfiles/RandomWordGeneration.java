a3ceecd96fffcf0e0457e4725b7e17ef8d93c08d
ThomasCanning <thomas500book@gmail.com>
2021-03-11 19:06:14 +0000

    Creating a class that generates random words from a text file of words, will be used in Hangman game

---
+++ src/workingwithfiles/RandomWordGeneration.java
@@ -0,0 +1,27 @@
+package workingwithfiles;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RandomWordGeneration {
+
+    private static int random(int listSize){  //Method for generating random number between 0 and list length
+        int randomNumber = (int) (Math.random() * (listSize+1));
+        return randomNumber;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("word_library.txt");  //reading file with words in
+        Scanner scan = new Scanner(file);
+        List<String> words = new ArrayList<String>();
+
+        for(int i = 0; scan.hasNextLine(); i++) {  //For loop to add all words in file to a list
+            words.add(i, scan.nextLine());
+        }
+
+        System.out.println(words.get(RandomWordGeneration.random(words.size())));
+                //Using random method to get a random word from the list of words
+    }
+}
