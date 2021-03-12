9e403eb3759b3d67cc1da68cdf5c0a300d6251ff
ThomasCanning <thomas500book@gmail.com>
2021-03-12 19:34:32 +0000

    Random Word Generation Method

--- src/workingwithfiles/RandomWordGeneration.java
+++ src/workingwithfiles/RandomWordGeneration.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class RandomWordGeneration {
+public class RandomWordGeneration {  //This class has a method that can be called which generates a random word from the .txt file "word_library.txt"
 
     private static int random(int listSize){  //Method for generating random number between 0 and list length
         int randomNumber = (int) (Math.random() * (listSize+1));
